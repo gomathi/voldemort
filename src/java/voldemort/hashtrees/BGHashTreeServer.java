52bec46a0280cb61f91db141d5abf8aa729e9a80
Gomathivinayagam Muthuvinayagam <gomes@yahoo-inc.com>
2014-08-14 23:02:12 -0700

    Adding copyright notice and updating BGStoppable class to use different logic in listening to stop signals

--- src/java/voldemort/hashtrees/BGHashTreeServer.java
+++ src/java/voldemort/hashtrees/BGHashTreeServer.java
@@ -1,4 +1,21 @@
+/*
+ * Copyright 2008-2014 LinkedIn, Inc
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
 package voldemort.hashtrees;
+
+import java.util.concurrent.CountDownLatch;
 
 import org.apache.log4j.Logger;
 import org.apache.thrift.server.TServer;
@@ -41,6 +58,12 @@ public class BGHashTreeServer extends BGStoppableTask {
     public void stop() {
         stopServer();
         super.stop();
+    }
+
+    @Override
+    public void stop(final CountDownLatch shutdownLatch) {
+        stopServer();
+        super.stop(shutdownLatch);
     }
 
     private static TServer createServer(int serverPortNo, HashTree hashTree)
